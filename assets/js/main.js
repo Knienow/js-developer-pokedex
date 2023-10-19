c2a7d77cd67fcebd62460ff11a36b1055e1d2308
Katia Nienow <nienowkatia@gmail.com>
2023-10-19 11:18:30 -0300

    inserindo o botão que exibe mais detalhes, como altura e peso de todos os pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,41 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+
+function convertPokemonToLiWithDetails(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <span class="name"> Altura: ${pokemon.height}</span>
+            <span class="name"> Peso: ${pokemon.weight}</span>
+            
+
+            
+            <div class="detail">
+            <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            
+            <img src="${pokemon.photo}"
+            alt="${pokemon.name}">
+            
+            </div>
+        </li>
+    `
+}
+
+
+function detailsPokemon(pokemons) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLiWithDetails).join('')
+        pokemonList.innerHTML = newHtml;
+    })
+}
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -18,6 +50,7 @@ function convertPokemonToLi(pokemon) {
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
+
             </div>
         </li>
     `
